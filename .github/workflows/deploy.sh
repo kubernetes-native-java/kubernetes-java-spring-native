7d2df036941c20afbe786f6c6d57a6bacb1bf233
Josh Long <josh@joshlong.com>
2021-04-13 21:08:50 -0700

    deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
-
-
-
-
 set -e
 set -o pipefail
-
-
-mvn verify package deploy
-
-echo "going to deploy..."
 export PROJECT_ID=${GCLOUD_PROJECT}
-
+echo "going to deploy..."
+mvn verify package deploy
 APP_NAME=spring-native-kubernetes
-
 kubectl get pods
