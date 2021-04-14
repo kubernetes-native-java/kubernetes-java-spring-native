65901d2044aab2fde8c50a57c5d28a9b5b10ff61
Josh Long <josh@joshlong.com>
2021-04-13 18:28:17 -0700

    gi

---
+++ src/main/java/booternetes/springnativekubernetes/SpringNativeKubernetesApplication.java
@@ -0,0 +1,13 @@
+package booternetes.springnativekubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/booternetes/springnativekubernetes/SpringNativeKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package booternetes.springnativekubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
