aef98c4970fc1e71528f389594a0689bf7901eeb
Josh Long <josh@joshlong.com>
2021-12-09 02:18:50 -0800

    move to spring native 0.11.x

---
+++ src/main/java/io/kubernetes/nativex/KubernetesApiNativeConfiguration.java
@@ -0,0 +1,69 @@
+package io.kubernetes.nativex;
+
+import com.google.gson.annotations.JsonAdapter;
+import io.swagger.annotations.ApiModel;
+import lombok.extern.log4j.Log4j2;
+import org.reflections.Reflections;
+import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
+import org.springframework.nativex.AotOptions;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.nativex.type.NativeConfiguration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.nativex.hint.TypeAccess.*;
+
+/**
+ * These hints are inspired by <a href="https://github.com/scratches/spring-controller">
+ * Dr. Dave Syer's sample Kubernetes controller</a> and the configuration therein.
+ * <p>
+ * These types work <a href="https://github.com/kubernetes-client/java">in conjunction
+ * with the autoconfiguration provided by the official Kubernetes Java client</a>, most of
+ * which is code-generated from Swagger. This support automatically registers any
+ * code-generated types that have {@link io.swagger.annotations.ApiModel} on it, limiting
+ * the registration to the code-generated types in the {@link io.kubernetes} package.
+ * <p>
+ * This hints class also registers options required to use this with a HTTPS API endpoints
+ * with custom character sets.
+ *
+ * @author Josh Long
+ * @author Dave Syer
+ */
+
+@NativeHint(//
+
+		options = { "-H:+AddAllCharsets", "--enable-all-security-services", "--enable-https", "--enable-http" },
+		types = { //
+				@TypeHint( //
+						access = { DECLARED_CLASSES, DECLARED_CONSTRUCTORS, DECLARED_FIELDS, DECLARED_METHODS }, //
+						typeNames = { "io.kubernetes.client.informer.cache.ProcessorListener",
+								"io.kubernetes.client.extended.controller.Controller",
+								"io.kubernetes.client.custom.IntOrString",
+								"io.kubernetes.client.custom.Quantity$QuantityAdapter",
+								"io.kubernetes.client.custom.IntOrString$IntOrStringAdapter",
+								"io.kubernetes.client.util.generic.GenericKubernetesApi$StatusPatch",
+								"io.kubernetes.client.util.Watch$Response" }) //
+		}//
+)
+@Log4j2
+public class KubernetesApiNativeConfiguration implements NativeConfiguration {
+
+	@Override
+	public void computeHints(NativeConfigurationRegistry registry, AotOptions aotOptions) {
+		Reflections reflections = new Reflections("io.kubernetes");
+		Set<Class<?>> apiModels = reflections.getTypesAnnotatedWith(ApiModel.class);
+		Set<Class<?>> jsonAdapters = reflections.getTypesAnnotatedWith(JsonAdapter.class);
+		Set<Class<?>> all = new HashSet<>();
+		all.addAll(jsonAdapters);
+		all.addAll(apiModels);
+		all.forEach(clzz -> {
+			if (log.isDebugEnabled()) {
+				log.debug(clzz.getName());
+			}
+			registry.reflection().forType(clzz).withAccess(values()).build();
+		});
+	}
+
+}
